34c3aff2af34c8b9fa69f8b2e447be697b976c87
manu-hn <manuu.hn11@gmail.com>
2024-06-29 13:11:32 +0530

    "Added react-icons dependency and updated layout.tsx with new div elements and styles"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-slate-50 text-gray-950`}>
+        <div className="bg-[#cdb2ca] absolute top-[-6rem] right-[11rem] h-[30rem] w-[30rem] rounded-full blur-[10rem]
+        sm:w-[68rem]">
+
+        </div>
+      
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[30rem] w-[50rem] -z-10 rounded-full blur-[10rem]
+        sm:w-[68rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]">
+
+        </div>
+      
        
         {children}
       
