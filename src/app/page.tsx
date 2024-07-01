8bb5c6b3075cf97067a881037cb0c3681abf3064
manu-hn <manuu.hn11@gmail.com>
2024-07-01 17:08:39 +0530

    "Update favicon, layout, page, and navbar components with various styling and structural changes"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 
 import AboutSection from "@/components/about";
+import Introduction from "@/components/intro";
 import ReduxProvider from "@/store/ReduxProvider";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <ReduxProvider>
-      <main className=" min-h-screen border-2 ">
-        {/* <AboutSection /> */}
+      <main className=" min-h-screen  ">
+        <Introduction />
       </main>
     </ReduxProvider>
   );
